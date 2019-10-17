b1e14ffeba9024d23ed605f670dbda80c5fd61c9
RubenV-Dev <r.will16@hotmail.com>
2019-10-17 16:43:39 -0400

    1

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 15, 'male', 'blue', 'chill', true );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 16, 'male', 'blue', 'sweet', true );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Rowdy', 17, 'blue', 'female', 'chill', true );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Tabitha', 18, 'pink', 'male', 'chill', false );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Seargant Brown', 19, 'female', 'yellow', 'chill', true );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Melissa', 20, 'orange', 'male', 'alright', true );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Grinch', 21, 'green', 'female', 'amazing', true );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES (?, 20, 'blue', 'male', 'hoorah', true );
